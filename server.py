2f497aa303c3855875fe7faadf457c2dc5813e1e
Microsoft Learn Student <learn@contoso.com>
2022-03-02 11:03:08 +0700

    Init heroku

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask,jsonify
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
-from json import dumps
+# from sqlalchemy import create_engine
+# from json import dumps
 
 
 app = Flask(__name__)
@@ -32,5 +32,4 @@ api.add_resource(Employees, '/employees') # Route_1
 # api.add_resource(Tracks, '/tracks') # Route_2
 # api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
 
-if __name__ == '__main__':
-     app.run()
+
